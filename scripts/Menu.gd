923d8f9d49aa2c9449a6e136dc04f86df601c887
EstudianteGenerico047 <carloarturomerinoc@gmail.com>
2020-07-28 12:45:17 -0400

    elementos extra añadidos

--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -8,8 +8,7 @@ func _on_Versus_pressed():
 	get_tree().change_scene("res://scenes/Character Select.tscn")
 
 func _on_Creditos_pressed():
-	pass # Replace with function body.
-
+	get_tree().change_scene("res://scenes/Credits.tscn")
 
 func _on_Quit_pressed():
 	get_tree().quit()
