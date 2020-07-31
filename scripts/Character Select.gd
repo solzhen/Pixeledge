057fd6fefc0473338066593712cafc9c03a2e2f4
EstudianteGenerico047 <carloarturomerinoc@gmail.com>
2020-07-31 00:23:31 -0400

    escape retorna menu principal

--- scripts/Character Select.gd
+++ scripts/Character Select.gd
@@ -19,6 +19,8 @@ func _ready():
 	pass # Replace with function body.
 
 func _input(event):
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().change_scene("res://scenes/Menu.tscn")
 	if event is InputEventMouseButton:
 		print("Mouse Click/Unclick at: ", event.position)
 		#print(event.button_index)
